eb9ec9f1b41329d4e678741999179c263d4128ec
AnonMuk <zubair.mukhi@gmail.com>
2019-11-12 11:19:47 -0500

    Now interactive!

--- Webscraper/webscraper.py
+++ Webscraper/webscraper.py
@@ -1,10 +1,19 @@
 import listmaker as scrape
 import time
 import datetime
+import sys
 
-for x in range(0, 100): #15 min between scan, 25 hours of scanning
-    print("Iteration " + datetime.datetime.now().strftime("%H:%M:%S"))
-    repoList = scrape.getAllValues()
-    scrape.exportToCSV("scraperresults.csv", repoList)
-    scrape.cleanupCSV("scraperresults.csv")
-    time.sleep(900)
+if(len(sys.argv) < 2):
+    print("Usage: python webscraper.py <number of hours>")
+else:
+    try:
+        time = int(sys.argv[1]) * 4
+        print(str(time) + " iterations!")
+        for x in range(1, time): #15 min between scan, 25 hours of scanning
+            print("Iteration " + str(x) + " at time " +  datetime.datetime.now().strftime("%H:%M:%S"))
+            repoList = scrape.getAllValues()
+            scrape.exportToCSV("scraperresults.csv", repoList)
+            scrape.cleanupCSV("scraperresults.csv")
+            time.sleep(900)
+    except (TypeError, ValueError) as e:
+        print("Please enter an integer")
